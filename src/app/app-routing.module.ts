6111c3c06c86c7d1c4ad0230df582d9eda469c8a
Mario <mario.martinez@secretescapes.com>
2017-11-17 13:13:07 +0100

    add more scaffolding

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,15 @@ import {RouterModule, Routes} from '@angular/router';
 import {NgModule} from '@angular/core';
 import {BoardsComponent} from './boards/boards.component';
 import {RootComponent} from './root/root.component';
+import {AuthComponent} from './auth/auth.component';
+import {LoginComponent} from './login/login.component';
+import {SignupComponent} from './signup/signup.component';
 const routes: Routes = [
   { path: '', redirectTo: '/app/boards', pathMatch: 'full' },
+  {path: 'auth', component: AuthComponent, children: [
+    { path: 'login', component: LoginComponent },
+    { path: 'signup', component: SignupComponent }
+  ]},
   {path: 'app', component: RootComponent, children: [
     { path: 'boards', component: BoardsComponent }
   ]}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,18 @@ import {environment} from '../environments/environment';
 import { BoardsComponent } from './boards/boards.component';
 import {AppRoutingModule} from './app-routing.module';
 import { RootComponent } from './root/root.component';
+import { AuthComponent } from './auth/auth.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BoardsComponent,
-    RootComponent
+    RootComponent,
+    AuthComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
